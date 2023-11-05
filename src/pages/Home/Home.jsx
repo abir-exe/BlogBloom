2f217657ebc0128b1cddbfacb9a1adc1c0f25b94
Abir <abirmahmud221@gmail.com>
2023-11-05 14:27:36 +0600

    added recent section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from "./Banner";
 import NewsLetter from "./NewsLetter";
+import Recent from "./Recent";
 
 
 
 const Home = () => {
     return (
-        <div>
+        <div className="space-y-10">
             <Banner></Banner>
+            <Recent></Recent>
             <NewsLetter></NewsLetter>
         </div>
     );
---
+++ src/pages/Home/Recent.jsx
@@ -0,0 +1,111 @@
+const Recent = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2 className="text-4xl text-center">Recent Blogs</h2>
+        </div>
+        <div className="grid grid-cols-3 gap-10 mb-5">
+            {/* <--card--> */}
+          <div className="card  bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://i.ibb.co/hBw19FZ/s5.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+            {/* <--card--> */}
+          <div className="card  bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://i.ibb.co/hBw19FZ/s5.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+            {/* <--card--> */}
+          <div className="card  bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://i.ibb.co/hBw19FZ/s5.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+            {/* <--card--> */}
+          <div className="card  bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://i.ibb.co/hBw19FZ/s5.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+            {/* <--card--> */}
+          <div className="card  bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://i.ibb.co/hBw19FZ/s5.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+            {/* <--card--> */}
+          <div className="card  bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://i.ibb.co/hBw19FZ/s5.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Recent;
